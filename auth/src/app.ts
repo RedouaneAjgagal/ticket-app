1c62f56906fbc787248870787264fcf55895886e
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-13 01:12:52 +0100

    [FEAT] connect to mongodb using mongo image

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import router from "./routes/main";
 import { errorHandler, notFound } from "./middlewares";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -12,7 +13,21 @@ app.use(router);
 app.use(notFound);
 app.use(errorHandler);
 
+
+
 const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/ticket-auth");
+        console.log("Mongo is connected!")
+    } catch (error) {
+        console.error(error)
+    }
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+start();
