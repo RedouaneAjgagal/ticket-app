3c44b02f45044cf81fa736f12911fb33b1407977
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-23 19:59:29 +0100

    [FEAT] fetch current user in serverSideProps to check if signed in or not

---
+++ client/api/index.ts
@@ -0,0 +1,5 @@
+import buildClient from "./build-client";
+
+export {
+    buildClient
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,44 @@
+import axios, { AxiosResponse } from "axios";
+import { GetServerSideProps } from "next";
+import { buildClient } from "../../api";
 
+interface CurrentUserResponse {
+  user: null | {
+    id: string;
+    email: string;
+    iat: number;
+    exp: number;
+  }
+}
 
-export default function Home() {
+function Home(props: React.PropsWithoutRef<CurrentUserResponse>) {
+  console.log(props.user);
+
   return (
-    <div>Home</div>
+    <div>
+      {props.user
+        ? (
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl">You are signed in</h1>
+            <p>email: {props.user.email}</p>
+          </div>
+        )
+        : (
+          <div>
+            <h1 className="text-3xl">You are NOT signed in</h1>
+          </div>
+        )
+      }
+    </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const client = buildClient(context);
+  const response: AxiosResponse<CurrentUserResponse> = await client.get("/api/users/current-user");
+  return {
+    props: response.data
+  };
+}
+
+export default Home;
