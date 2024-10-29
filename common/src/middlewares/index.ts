e946dac591225cb956d69dafce788746416f1f59
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-30 00:12:03 +0100

    [FEAT] add a validate request middleware

--- common/src/middlewares/index.ts
+++ common/src/middlewares/index.ts
@@ -2,12 +2,14 @@ import authentication from "./authentication";
 import currentUser, { IUserPayload, IUserPayloadToken } from "./current-user";
 import errorHandler from "./error-handler";
 import notFound from "./not-found";
+import validateRequest from "./validateRequest";
 
 export {
     errorHandler,
     notFound,
     currentUser,
     authentication,
+    validateRequest,
     IUserPayload,
     IUserPayloadToken
 }
