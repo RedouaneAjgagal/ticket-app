f47d4b8b7702589b190bb7948315eae0d2be8141
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-31 00:35:57 +0100

    [FEAT] handle errors for invalid mongodb IDs and duplicated values

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -9,6 +9,24 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
         return;
     }
 
+    if (err && err.code === 1100) {
+        res.status(400).json({
+            errors: [{
+                message: "Duplicated value entered, please choose another value"
+            }]
+        });
+        return;
+    }
+
+    if (err && err.name === "CastError") {
+        res.status(404).json({
+            errors: [{
+                message: err.value ? `No item found with ID ${err.value}` : "No item found"
+            }]
+        });
+        return;
+    }
+
     res.status(500).json({
         errors: [{
             message: "Something went wrong"
