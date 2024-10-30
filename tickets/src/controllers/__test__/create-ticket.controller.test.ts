7fdb6fe789096db2d0eb3d59ce561e032da30669
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-30 02:11:33 +0100

    [TEST] should create a ticket and insert it in the db

--- tickets/src/controllers/__test__/create-ticket.controller.test.ts
+++ tickets/src/controllers/__test__/create-ticket.controller.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "../../app";
+import { Ticket } from "../../models";
 
 it("should not return a 404 when trying to create a ticket as a post request", async () => {
     const response = await request(app)
@@ -67,4 +68,29 @@ it("should throw an error if the ticket price is invalid or not provided", async
             title: "a ticket title"
         })
         .expect(400);
+});
+
+it("should create a ticket with valid inputs and insert it in the database", async () => {
+    const { cookie } = global.signin();
+
+    let tickets = await Ticket.find({});
+    const INITIAL_START = 0;
+    expect(tickets.length).toEqual(INITIAL_START);
+
+    const body = {
+        title: "a ticket title",
+        price: 10
+    };
+
+    await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send(body)
+        .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(INITIAL_START + 1);
+    expect(tickets[INITIAL_START].title).toEqual(body.title);
+    expect(tickets[INITIAL_START].price).toEqual(body.price);
+    expect(tickets[INITIAL_START].userId).toBeTruthy();
 });
