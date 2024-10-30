76512cc8421f930d8a08adc2fb671683d5474019
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-10-30 02:20:06 +0100

    [FEAT] create a ticket

--- tickets/src/controllers/create-ticket.controller.ts
+++ tickets/src/controllers/create-ticket.controller.ts
@@ -1,14 +1,20 @@
-import { validationResult } from "express-validator";
 import { RequestHandler } from "express";
-import { RequestValidationError } from "@redagtickets/common";
+import { Ticket } from "../models";
 
 /**
  * create a new ticket controller
  * @param req 
  * @param res 
  */
-const createTicketController: RequestHandler = (req, res) => {
-    res.sendStatus(201);
+const createTicketController: RequestHandler = async (req, res) => {
+    const { title, price } = req.body;
+    const ticket = await Ticket.build({
+        title,
+        price,
+        userId: req.user!.id
+    });
+
+    res.status(201).json(ticket);
 };
 
 export default createTicketController
