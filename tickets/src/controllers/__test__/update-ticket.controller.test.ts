3ccb767d0731d48045663a8f7b98ddafd500325d
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-01 01:28:14 +0100

    [TEST] update ticket

---
+++ tickets/src/controllers/__test__/update-ticket.controller.test.ts
@@ -0,0 +1,145 @@
+import request from "supertest";
+import app from "../../app";
+import mongoose from "mongoose";
+
+const validInputs = {
+    title: "a valid ticket title",
+    price: 10
+};
+
+const invalidInputs = {
+    title: "",
+    price: -10
+};
+
+it("should find the route for updating the ticket", async () => {
+    const { cookie } = global.signin();
+
+    const ticketId = new mongoose.Types.ObjectId().toHexString();
+
+    const response = await request(app)
+        .put(`/api/tickets/${ticketId}`)
+        .set("Cookie", cookie)
+        .send(validInputs);
+
+    expect(response.status).not.toBe(404);
+});
+
+it("should return 401 if the user is not signed in", async () => {
+    const ticketId = new mongoose.Types.ObjectId().toHexString();
+
+    await request(app)
+        .put(`/api/tickets/${ticketId}`)
+        .send(validInputs)
+        .expect(401);
+});
+
+it("should have a valid ticket id for updating the ticket", async () => {
+    const { cookie } = global.signin();
+
+    await request(app)
+        .put("/api/tickets/invalidId")
+        .set("Cookie", cookie)
+        .send(validInputs)
+        .expect(400);
+});
+
+it("should return a 400 if the provided ticket is not exist", async () => {
+    const { cookie } = global.signin();
+
+    const ticketId = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+        .put(`/api/tickets/${ticketId}`)
+        .set("Cookie", cookie)
+        .send(validInputs)
+        .expect(400);
+});
+
+it("should ticket belong to the current user", async () => {
+    const { cookie: signedinUserCookie } = global.signin();
+    const { cookie: createdTicketUserCookie } = global.signin();
+
+    const createTicketResponse = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", createdTicketUserCookie)
+        .send(validInputs)
+        .expect(201);
+
+    const updatedTicketInputs = {
+        title: "updated ticket title",
+        price: 99
+    }
+
+    // update ticket request
+    await request(app)
+        .put(`/api/tickets/${createTicketResponse.body.id}`)
+        .set("Cookie", signedinUserCookie)
+        .send(updatedTicketInputs)
+        .expect(401);
+
+    const getTicketResponse = await request(app)
+        .get(`/api/tickets/${createTicketResponse.body.id}`)
+        .send({})
+        .expect(200);
+
+    expect(getTicketResponse.body.title).toEqual(validInputs.title);
+    expect(getTicketResponse.body.price).toEqual(validInputs.price);
+});
+
+it("should retun a 400 if its invalid inputs", async () => {
+    const { cookie } = global.signin();
+
+    const createTicketResponse = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send(validInputs)
+        .expect(201);
+
+    await request(app)
+        .put(`/api/tickets/${createTicketResponse.body.id}`)
+        .set("Cookie", cookie)
+        .send({
+            title: invalidInputs.title,
+            price: validInputs.price
+        })
+        .expect(400);
+
+    await request(app)
+        .put(`/api/tickets/${createTicketResponse.body.id}`)
+        .set("Cookie", cookie)
+        .send({
+            title: validInputs.title,
+            price: invalidInputs.price
+        })
+        .expect(400);
+});
+
+it("should update the ticket details", async () => {
+    const { cookie } = global.signin();
+
+    const createTicketResponse = await request(app)
+        .post("/api/tickets")
+        .set("Cookie", cookie)
+        .send(validInputs)
+        .expect(201);
+
+    const updatedTicketInputs = {
+        title: "updated ticket title",
+        price: 99
+    }
+
+    // updated ticket request
+    await request(app)
+        .put(`/api/tickets/${createTicketResponse.body.id}`)
+        .set("Cookie", cookie)
+        .send(updatedTicketInputs)
+        .expect(200);
+
+    const getTicketResponse = await request(app)
+        .get(`/api/tickets/${createTicketResponse.body.id}`)
+        .send({})
+        .expect(200);
+
+    expect(getTicketResponse.body.title).toEqual(updatedTicketInputs.title);
+    expect(getTicketResponse.body.price).toEqual(updatedTicketInputs.price);
+});
