895b302adbdf295efdd11ff6d04f1c4251224832
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-02 23:37:53 +0100

    [FIX] import create/update ticket events

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -1,9 +1,13 @@
 import Subjects from "./subjects";
 import Publisher from "./publisher";
 import Listener from "./listener";
+import TicketCreatedEvent from "./tickets/ticket-created-event";
+import TicketUpdatedEvent from "./tickets/ticket-updated-event";
 
 export {
     Subjects,
     Publisher,
-    Listener
+    Listener,
+    TicketCreatedEvent,
+    TicketUpdatedEvent
 }
