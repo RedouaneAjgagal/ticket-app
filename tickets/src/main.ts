4b973158c57b1e21dfdf38f51d61dd9a444c5383
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-04 23:48:57 +0100

    [REFACTOR] use nats keys inside k8s deployment

--- tickets/src/main.ts
+++ tickets/src/main.ts
@@ -5,11 +5,18 @@ import natsWrapper from "./nats-wrapper";
 
 const PORT = 3000
 const start = async () => {
-    if (!process.env.JWT_SECRET) throw new Error("JWT Secret is required");
-    if (!process.env.MONGO_URI) throw new Error("Mongo URI is required");
+    if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is required");
+    if (!process.env.MONGO_URI) throw new Error("MONGO_URI is required");
+    if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID is required");
+    if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID is required");
+    if (!process.env.NATS_URL) throw new Error("NATS_URL is required");
 
     try {
-        await natsWrapper.connect("ticketing", "123", "http://nats-srv:4222");
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL,
+        );
 
         // handle closing connection
         natsWrapper.stan.on("close", () => {
