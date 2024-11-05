c00cbddba971e3cd6ff7f17280d6a4fd113d81f0
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-05 22:06:12 +0100

    [FEAT] set order status enums for common package for other services

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -3,11 +3,13 @@ import Publisher from "./publisher";
 import Listener from "./listener";
 import TicketCreatedEvent from "./tickets/ticket-created-event";
 import TicketUpdatedEvent from "./tickets/ticket-updated-event";
+import OrderStatus from "./types/order-status";
 
 export {
     Subjects,
     Publisher,
     Listener,
     TicketCreatedEvent,
-    TicketUpdatedEvent
+    TicketUpdatedEvent,
+    OrderStatus
 }
---
+++ common/src/events/types/order-status.ts
@@ -0,0 +1,9 @@
+enum OrderStatus {
+    Created = "created",
+    Cancelled = "cancelled",
+    Expired = "expired",
+    AwaitingPayment = "awaiting:payment",
+    Completed = "completed"
+};
+
+export default OrderStatus;
