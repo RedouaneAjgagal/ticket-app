d6e54bc25473377b15a74aeeb95e2e3dceeb990c
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-06 23:36:58 +0100

    [FEAT] get current user orders

--- orders/src/controllers/get-orders.controller.ts
+++ orders/src/controllers/get-orders.controller.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+import { Order } from "../models";
 
 /**
  * Get all user's orders details controller
@@ -6,7 +7,11 @@ import { RequestHandler } from "express";
  * @param res 
  */
 const getOrdersController: RequestHandler = async (req, res) => {
-    res.sendStatus(200);
+    const orders = await Order.find({
+        userId: req.user!.id
+    }).populate("ticket");
+
+    res.status(200).json({ orders });
 };
 
 export default getOrdersController;
