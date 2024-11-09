8e6164f4bb42d4f10ebd67ca76d64b0905974b24
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-10 00:54:51 +0100

    [TEST] should implement OCC for the order model

---
+++ orders/src/models/__test__/order.test.ts
@@ -0,0 +1,52 @@
+import { OrderStatus } from "@redagtickets/common";
+import Order from "../order";
+import Ticket from "../ticket";
+
+it("should implements optimistic Concurrency control", async () => {
+    const ticket = await Ticket.build({
+        __v: 0,
+        title: "a ticket",
+        price: 10
+    });
+
+    const order = await Order.build({
+        userId: "123",
+        status: OrderStatus.Created,
+        ticket
+    });
+
+    const firstInstance = await Order.findById(order.id);
+    const secondInstance = await Order.findById(order.id);
+
+    firstInstance!.set("status", OrderStatus.AwaitingPayment);
+    await firstInstance!.save();
+
+    secondInstance!.set("status", OrderStatus.Cancelled);
+    try {
+        await secondInstance!.save();
+    } catch (error) {
+        return;
+    };
+
+    throw new Error("Doesn't implement optimistic concurrency control");
+});
+
+it('should increment the version number on each save', async () => {
+    const ticket = await Ticket.build({
+        __v: 0,
+        title: "first ticket",
+        price: 10
+    });
+
+    const order = await Order.build({
+        userId: "123",
+        status: OrderStatus.Created,
+        ticket
+    });
+
+    expect(order.__v).toEqual(0);
+
+    order.set("status", OrderStatus.Expired);
+    await order.save();
+    expect(order.__v).toEqual(1);
+});
