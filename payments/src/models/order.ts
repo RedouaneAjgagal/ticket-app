1edef55d66b8a8ea7d7d30af84d6096b42dc6ca5
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-17 23:13:30 +0100

    [FEAT] setup initial order model for the payment service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,72 @@
+import { OrderStatus } from "@redagtickets/common";
+import mongoose from "mongoose";
+
+interface OrderAttrs {
+    id: string;
+    __v: number;
+    userId: string;
+    status: OrderStatus;
+    ticket: {
+        id: string;
+        price: number;
+    };
+    createdAt: Date;
+};
+
+interface OrderDoc extends mongoose.Document<mongoose.Types.ObjectId> {
+    __v: number;
+    userId: string;
+    status: OrderStatus;
+    ticket: {
+        id: string;
+        price: number;
+    };
+};
+
+interface OrderStatics extends mongoose.Model<OrderDoc> {
+    build(attrs: OrderAttrs): Promise<OrderDoc>;
+};
+
+const orderSchema = new mongoose.Schema<OrderAttrs>({
+    userId: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: Object.values(OrderStatus),
+        required: true
+    },
+    ticket: {
+        id: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        }
+    },
+    createdAt: {
+        type: mongoose.Schema.Types.Date,
+        required: true
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+        }
+    }
+});
+
+orderSchema.statics.build = async (attrs: OrderAttrs) => {
+    const order = new Order(attrs);
+    await order.save();
+
+    return order;
+};
+
+const Order = mongoose.model<OrderAttrs, OrderStatics>("Order", orderSchema);
+
+export default Order;
