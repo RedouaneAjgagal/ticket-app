a20a180561cc59cd3dee53b866f202bec600b8a5
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-11-23 00:44:03 +0100

    [FEAT] order a ticket

--- client/src/pages/tickets/[ticketId].tsx
+++ client/src/pages/tickets/[ticketId].tsx
@@ -2,6 +2,9 @@ import { GetServerSideProps } from "next";
 import { ICurrentUser } from "../_app"
 import { buildClient } from "@/api";
 import { AxiosResponse } from "axios";
+import { useRequest } from "@/hooks";
+import { useRouter } from "next/router";
+import { Errors } from "@/components";
 
 interface Ticket {
     id: string;
@@ -19,17 +22,28 @@ interface ITicketShow extends ICurrentUser {
 }
 
 const TicketShow = (props: React.PropsWithoutRef<ITicketShow>) => {
-    const orderTicketHandler = () => {
-        console.log({ ticketId: props.ticket.id });
+    const router = useRouter();
+    const { errors, fetchData } = useRequest({
+        method: "post",
+        url: "/api/orders",
+        payload: {
+            ticketId: props.ticket.id
+        },
+        onSuccess: (data) => router.push(`/orders/${(data as { id: string }).id}`)
+    });
+
+    const orderTicketHandler = async () => {
+        await fetchData();
     }
 
     return (
-        <section className="py-8">
+        <section className="py-8 flex flex-col gap-8">
             <div className="flex flex-col gap-4 text-stone-800">
                 <h1 className="text-4xl font-semibold">{props.ticket.title}</h1>
                 <span className="text-xl font-medium">Price: ${props.ticket.price.toFixed(2)}</span>
                 <button onClick={orderTicketHandler} className="self-start py-2 px-3 bg-blue-500 font-medium text-white rounded">Purchase</button>
             </div>
+            <Errors errors={errors} />
         </section>
     )
 };
